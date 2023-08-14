129dd06e288e853d6993ea37eb3cf769d4f404d3
cailtondev <casiooliveyracosta@gmail.com>
2023-08-13 22:48:33 -0300

    project architecture organization

---
+++ src/templates/home/index.js
@@ -0,0 +1,13 @@
+import Navbar from "./../../components/navbar";
+
+function Home() {
+  return (
+    <div className="Home">
+      <nav className="navbar navbar-expand-lg bg-dark">
+        <Navbar />
+      </nav>
+    </div>
+  );
+}
+
+export default Home;
